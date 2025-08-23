d29c26df77072ae629f827eb4c7e9c4d6f88df0b
Manuel <manuelinfosec@gmail.com>
2025-08-23 05:06:35 +0100

    cpu_add_256: appending 256-bit digits to unisgned results

--- src/main.cu
+++ src/main.cu
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2023 MrSpike63
+    Copyright (C) 2025 Manuel
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
